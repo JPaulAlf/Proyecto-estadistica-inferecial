fa0411cd67b247aeccc46c55cf62933d6dfe79fc
Diego <Juandiegoa907@gmail.com>
2021-11-16 22:53:32 -0600

    sonido implementado y corrección de errores menores

--- logica-estadistica.js
+++ logica-estadistica.js
@@ -3,29 +3,36 @@ const z = 1.64;
 
 var n1, n2, e1, e2, p1, p2, resultMenor, resultMayor;
 
-obtenerDatos=()=>{
+obtenerDatos = () => {
     n1 = Number(document.getElementById('muestra1').value);
     n2 = Number(document.getElementById('muestra2').value);
     e1 = document.getElementById('exitos1').value;
     e2 = document.getElementById('exitos2').value;
 
-    
 
-    p1= Number(e1)/n1;
-    p2= Number(e2)/n2;
-    
+
+    p1 = Number(e1) / n1;
+    p2 = Number(e2) / n2;
+
 
 };
 
-calcularDatos=()=>{
+calcularDatos = () => {
     obtenerDatos();
-  
-    resultMenor=(((p1>p2)?p1:p2)-((p1<p2)?p1:p2))-z*(Math.sqrt((p1*(p1-1)/n1)+(p2*(p2-1)/n2)));
-    resultMayor=(((p1>p2)?p1:p2)-((p1<p2)?p1:p2))+z*(Math.sqrt((p1*(p1-1)/n1)+(p2*(p2-1)/n2)));
+    var audio = new Audio('click.mp3');
+    audio.play();
+    resultMenor = (((p1 > p2) ? p1 : p2) - ((p1 < p2) ? p1 : p2)) - z * (Math.sqrt((p1 * (1 - p1) / n1) + (p2 * (1 - p2) / n2)));
+    resultMayor = (((p1 > p2) ? p1 : p2) - ((p1 < p2) ? p1 : p2)) + z * (Math.sqrt((p1 * (1 - p1) / n1) + (p2 * (1 - p2) / n2)));
 
-    document.getElementById('porcentajeHombre').innerHTML=p1;
-    document.getElementById('porcentajeMujer').innerHTML=p2;
-    document.getElementById('limiteInferior').innerHTML=resultMenor;
-    document.getElementById('limiteSuperior').innerHTML=resultMayor;
-    document.getElementById('respuesta').innerHTML='El porcentaje para los hombres es'+p1+'% y para las mujeres es'+p2+'%. Con respecto a la estimación de la proporción con un intervalo de confianza del 90% tenemos que distan entre'+resultMenor+' a '+resultMayor;
+    resultMayor = resultMayor.toFixed(4);
+    resultMenor = resultMenor.toFixed(4);
+    p1 = p1.toFixed(4) * 100;
+    p2 = p2.toFixed(4) * 100;
+
+
+    document.getElementById('porcentajeHombre').innerHTML = p1;
+    document.getElementById('porcentajeMujer').innerHTML = p2;
+    document.getElementById('limiteInferior').innerHTML = resultMenor;
+    document.getElementById('limiteSuperior').innerHTML = resultMayor;
+    document.getElementById('respuesta').innerHTML = 'El porcentaje para los hombres es ' + p1 + '% y para las mujeres es ' + p2 + '%. Con respecto a la estimación de la proporción con un intervalo de confianza del 90% tenemos que distan entre ' + resultMenor + ' a ' + resultMayor;
 };
